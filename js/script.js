562c7f81ba4cd45ec5523c412b3c2521b91e9e2e
marco sartori <marcosartori98@yahoo.com>
2020-12-11 15:23:16 +0100

    finish js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-alert ('hello');
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito19
+
+// VARIABILI
+var nome;
+var cognome;
+var colore;
+
+
+// VALORI
+nome = prompt('Inserisci il nome');
+cognome = prompt('Inserisci il cogome');
+colore = prompt('Inserisci il tuo colore preferito');
+
+console.log(nome);
+console.log(cognome);
+console.log(colore);
+
+
+// VEDO SU HTML
+document.getElementById('codice').innerHTML= nome + cognome + colore + 19;
+
+// TUTTO IN MINUSCOLO
+nomeMinuscolo = nome.toLowerCase();
+cognomeMinuscolo = cognome.toLowerCase();
+coloreMinuscolo = colore.toLowerCase();
+
+// VEDO SU HTML
+document.getElementById('codice').innerHTML= nomeMinuscolo + cognomeMinuscolo + coloreMinuscolo + 19;
